a80d853279b192b80b15e5a04b1426164712ccfd
florian lueers <flo.lueers+github@gmail.com>
2024-10-15 22:44:43 +0300

    ????

--- malAlleChecksInFolge.py
+++ malAlleChecksInFolge.py
@@ -1,25 +1,36 @@
+# Zum Testen von Existenz von Acroba
 import os
-import re
-import subprocess
 
 cwd = os.getcwd()
 foldername = "/testfolder"
 
-windows = True
-alreadyInstalled = True if os.path.isdir(cwd + foldername) else False 
-wslInstalled = False
-dockerInstalled = False
-gitInstalled = False
 
-if windows:
-    try:
-        ans = subprocess.Popen(["wsl", "cat", "/proc/version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        output, errors = ans.communicate()
-        match = re.search(r'WSL(\d+)', output)
-        if int(match.group(1)) == 2:
-            wslInstalled = True
-    except:
-        pass
+# Fragen, ob Linux oder Windows
+import tkinter as tk
+
+def setWindows():
+    global windows
+    windows = True
+
+windows = False
+
+root = tk.Tk()
+
+windowsbutton = tk.Button(root, text = "Windows", command=setWindows)
+windowsbutton.pack()
+
+linuxbutton = tk.Button(root, text="Linux")
+linuxbutton.pack()
+
+root.mainloop()
+
+
+# Checken, ob git und docker installiert sind
+import subprocess
+import re
+
+gitInstalled = False
+dockerInstalled = False
 
 try:
     ans = subprocess.Popen(["git", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -37,24 +48,19 @@ try:
 except:
     pass
 
-print(windows, alreadyInstalled, gitInstalled, dockerInstalled, wslInstalled)
 
+# Check, ob WSL installiert ist
+windows = True
+wslInstalled = False
 
+if windows:
+    try:
+        ans = subprocess.Popen(["wsl", "cat", "/proc/version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        output, errors = ans.communicate()
+        match = re.search(r'WSL(\d+)', output)
+        if int(match.group(1)) == 2:
+            wslInstalled = True
+    except:
+        pass
 
-# sudo apt install git-all
-
-# sudo apt-get update
-# sudo apt-get install ca-certificates curl
-# sudo install -m 0755 -d /etc/apt/keyrings
-# sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-# sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# # Add the repository to Apt sources:
-# echo \
-#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-#   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-#   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo apt-get update
-
-# sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
+print(wslInstalled)
