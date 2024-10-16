a7d887ba0a6690f59ae3b47cfdc97df8c8615b8d
florian lueers <flo.lueers+github@gmail.com>
2024-10-16 09:47:44 +0300

    dockerInstallation in oneLine

--- installDockerAndGit.py
+++ installDockerAndGit.py
@@ -1,30 +1,19 @@
 import subprocess
 
+# sudo usermod -aG docker $USER
 def installDocker(windows=False):
-    listOfCommands = [
-        "sudo apt-get update",
-        "sudo apt-get install -y ca-certificates curl",
-        "sudo mkdir -m 0755 -p /etc/apt/keyrings",
-        "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc",
-        "sudo chmod a+r /etc/apt/keyrings/docker.asc",
-        'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $(VERSION_CODENAME)) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null',
-        "sudo apt-get update",
-        "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
-    ]
-    
-    for command in listOfCommands:
-        if windows:
-            # Füge "wsl" am Anfang des Befehls hinzu
-            command = f"wsl {command}"
-        
-        try:
-            print(f"Ausführe: {command}")
-            ans = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
-            print(ans.stdout)
-            if ans.stderr:
-                print(f"Fehler: {ans.stderr}")
-        except Exception as e:
-            print(f"Ein Fehler ist aufgetreten: {e}")
+    command = "curl -sSL https://get.docker.com/ | sh"
+    if windows:
+        # Füge "wsl" am Anfang des Befehls hinzu
+        command = f"wsl curl -sSL https://get.docker.com/ | sh"
+    try:
+        print(f"Ausführe: {command}")
+        ans = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+        print(ans.stdout)
+        if ans.stderr:
+            print(f"Fehler: {ans.stderr}")
+    except Exception as e:
+        print(f"Ein Fehler ist aufgetreten: {e}")
 
 # Beispielaufruf
 installDocker(windows=True)
